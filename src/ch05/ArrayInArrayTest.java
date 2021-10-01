4874acdd53c468acb083b70605763125f6957ddd
sua-h <suaah.96@gmail.com>
2021-10-01 17:02:55 +0900

    211001

--- src/ch05/ArrayInArrayTest.java
+++ src/ch05/ArrayInArrayTest.java
@@ -18,10 +18,10 @@ public class ArrayInArrayTest {
         };
 
         String[] names = {"김영희", "김철수", "홍길동"};
-        int[] stuScore = new int[names.length];     // 학생 평균 점수
+        int[] stuScore = new int[names.length];
 
         String[] clsArr = {"수학", "영어", "국어"};
-        int[] clsScore = new int[clsArr.length];   // 과목 평균 점
+        int[] clsScore = new int[clsArr.length];
 
 
         for (int i = 0; i < scores.length; i++) {
@@ -32,11 +32,11 @@ public class ArrayInArrayTest {
             }
         }
 
-        for(int i=0; i<stuScore.length; i++) {
+        for(int i = 0; i < stuScore.length; i++) {
             System.out.printf("%s 총점: %d점, 평균: %.1f점\n", names[i], stuScore[i]
                     , (float)stuScore[i] / clsArr.length);
         }
-        for(int i=0; i<clsScore.length; i++) {
+        for(int i = 0; i < clsScore.length; i++) {
             System.out.printf("%s 총점: %d점, 평균: %.1f점\n", clsArr[i], clsScore[i]
                     , (float)clsScore[i] / names.length);
         }
